d9c023b1785bea584d76f7b81dc408fb34fb0333
Abel Sassu <relinquished@hotmail.it>
2023-12-20 16:46:28 +0100

    init

---
+++ src/components/BookList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import books from "../data/books.json"
+import SingleBook1 from './SingleBook1';
+import { Col, Form, Row, Container } from "react-bootstrap";
+
+
+class BookList1 extends React.Component {
+    state = { search: "" };
+
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label className="label">
+                                Il peso della cultura...
+                            </Form.Label>
+                            <Form.Control
+                                className='search'
+                                type="text"
+                                placeholder="Cerca la tua arma qui!"
+                                value={this.state.searchQuery}
+                                onChange={(e) =>
+                                    this.setState({
+                                        search: e.target.value,
+                                    })
+                                }
+                            />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    {books.fantasy.map((book) => {
+                        return <SingleBook1 b={book} />;
+                    })}
+                </Row>
+            </Container>
+        );
+    }
+}
+export default BookList1;
